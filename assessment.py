7d385a0adb7513b24b6fbcd86f15c8d5b8f76b38
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-03 15:11:49 -0500

    Added dictionary and if statement to verify item_cost is less than money_paid

--- assessment.py
+++ assessment.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3.12
 def exact_change(item_cost, money_paid):
-    return f"{item_cost} {money_paid}"
+    str_response = "Your total is "
 
-print(exact_change(53.73, 100))
+    dct_currency = {
+        "One Hundred Dollar Bill" : 100,
+        "Fifty Dollar Bill" : 50,
+        "Twenty Dollar Bill" : 20,
+        "Ten Dollar Bill" : 10,
+        "Five Dollar Bill" : 5,
+        "Two Dollar Bill" : 2,
+        "One Dollar Bill" : 1,
+        "Quarter" : 0.5,
+        "Dime" : 0.1,
+        "Nickle" : 0.05,
+        "Penny" : 0.01,
+    }
+
+    if item_cost < money_paid :
+        return f"{str_response} {dct_currency}"
+    else :
+        return "You can't afford this item." 
+
+#print(exact_change(53.73, 100))
+print(exact_change(10.0, 3.00))
